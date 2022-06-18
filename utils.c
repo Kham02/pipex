a13b2880db9c13012b03a7cf7451203b9c6aa7d0
estrong <79113244+Kham02@users.noreply.github.com>
2022-06-18 14:29:54 +0400

    bon

--- utils.c
+++ utils.c
@@ -12,6 +12,21 @@
 
 #include "pipex.h"
 
+int	ft_strcmp(const char *s1, const char *s2)
+{
+	size_t	i;
+
+	i = -1;
+	if (!s1 && !s2)
+		return (0);
+	else if (!s1 || !s2)
+		return (1);
+	while (s1[++i] || s2[i])
+		if (s1[i] != s2[i])
+			return (1);
+	return (0);
+}
+
 void	error(char *s)
 {
 	return (perror(s));
